9923266b5e7c4f0b7e087fc86c8f1c4ce31cfa03
salesluis <salesluis434@gmail.com>
2026-03-02 21:48:48 -0300

    Feat: Add suport Controllers

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
+using scheduling_management.Aplication.Services.Token;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<TokenService>();
+
 var app = builder.Build();
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
